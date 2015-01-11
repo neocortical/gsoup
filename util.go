ff8b3661d11ceb1862dfcdef0a22ba9a29fd66cc
Nathan Smith <nate@branchout.com>
2015-01-11 15:06:41 -0800

    fixed panic

--- util.go
+++ util.go
@@ -1,6 +1,9 @@
 package gsoup
 
+import "golang.org/x/net/html/atom"
+
 func cloneWhitelist(in whitelist) (out whitelist) {
+	out = make(map[atom.Atom]Tagdef)
 	for tag, tagdef := range in {
 		newdef := Tagdef{Tag: tagdef.Tag, AllowedAttrs: make(Attrset)}
 		for attr := range tagdef.AllowedAttrs {
